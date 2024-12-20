39482400778476b800f372cb666a4ff13bbfcdc4
Abarna Subramaniyan <112686551+abarna10303@users.noreply.github.com>
2024-12-21 02:53:19 +0530

    Update agriTechService.js

--- services/agriTechService.js
+++ services/agriTechService.js
@@ -1,6 +1,11 @@
 import { MongoClient } from "mongodb";
+import twilio from "twilio";
+import dotenv from "dotenv";
+dotenv.config();
 const url =
   "mongodb+srv://prabaharanvasu96:prabaharanvasu96@cluster1.erxhy.mongodb.net/";
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = new MongoClient(url);
 export const getAgriTechService = async () => {
   try {
@@ -11,6 +16,62 @@ export const getAgriTechService = async () => {
     const result = await collection.find({}).toArray();
     console.log("Data fetch succesful");
     return result;
+  } catch (error) {
+    console.error("Error  occurred while reading from MongoDB:", error);
+    throw error;
+  } finally {
+    await client.close();
+  }
+};
+
+export const otpLoginService = async (mobileNo) => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const database = client.db("agri_tech");
+    const collection = database.collection("users");
+    const result = await collection
+      .find({ mobileNo }, { projection: { _id: 0 } })
+      .toArray();
+    if (result !== null) {
+      const clientMessage = twilio(accountSid, authToken);
+      const otp = Math.floor(100000 + Math.random() * 900000);
+      clientMessage.messages.create({
+        body: `Your OTP is ${otp}`,
+        from: "+12184838661",
+        to: `+91${mobileNo}`,
+      });
+      await collection.updateOne({ mobileNo }, { $set: { otp } });
+    } else {
+      throw new Error("No data found");
+    }
+    console.log("Data fetch succesful");
+    return result;
+  } catch (error) {
+    console.error("Error  occurred while reading from MongoDB:", error);
+    throw error;
+  } finally {
+    await client.close();
+  }
+};
+
+export const otpLoginValidationService = async (mobileNo, otp) => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const database = client.db("agri_tech");
+    const collection = database.collection("users");
+    const numberOtp = Number(otp);
+    const result = await collection.findOne(
+      { mobileNo, otp: numberOtp },
+      { projection: { _id: 0 } }
+    );
+
+    if (result !== null) {
+      return result;
+    } else {
+      throw new Error("No data found");
+    }
   } catch (error) {
     console.error("Error  occurred while reading from MongoDB:", error);
     throw error;
@@ -41,6 +102,26 @@ export const postUserDataService = async (data) => {
     return result;
   } catch (error) {
     console.error("Error  occurred while reading from MongoDB:", error);
+    throw error;
+  } finally {
+    await client.close();
+  }
+};
+
+export const postJobDetailsService = async (data) => {
+  try {
+    await client.connect();
+
+    const database = client.db("agri_tech");
+    const collection = database.collection("Job_list");
+
+    // Directly insert the received data
+    const result = await collection.insertOne(data);
+
+    console.log("Data inserted successfully:", result);
+    return result;
+  } catch (error) {
+    console.error("Error occurred while inserting into MongoDB:", error);
     throw error;
   } finally {
     await client.close();
