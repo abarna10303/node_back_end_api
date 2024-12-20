d1497c78c8dca03c3c859995752e78301d31ba2d
Abarna Subramaniyan <112686551+abarna10303@users.noreply.github.com>
2024-12-21 02:52:34 +0530

    Update agriTechController.js

--- controllers/agriTechController.js
+++ controllers/agriTechController.js
@@ -1,6 +1,9 @@
 import {
   getAgriTechService,
+  otpLoginService,
+  otpLoginValidationService,
   postUserDataService,
+  postJobDetailsService,
 } from "../services/agriTechService.js";
 
 export const agriTechController = async (req, res) => {
@@ -15,9 +18,48 @@ export const agriTechController = async (req, res) => {
   }
 };
 
+export const otpLoginController = async (req, res) => {
+  try {
+    const { mobileNo } = req.body;
+    const data = await otpLoginService(mobileNo);
+    res.json({
+      flag: "success",
+      data,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const otpLoginValidationController = async (req, res) => {
+  try {
+    const { mobileNo, otpNo } = req.body;
+    const data = await otpLoginValidationService(mobileNo, otpNo);
+
+    res.json({
+      flag: "success",
+      data,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 export const postUserDataController = async (req, res) => {
   try {
     const data = await postUserDataService(req.body);
+    res.json({
+      flag: "success",
+      data,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const postJobDetailsController = async (req, res) => {
+  try {
+    const data = await postJobDetailsService(req.body.data);
     res.json({
       flag: "success",
       data,
