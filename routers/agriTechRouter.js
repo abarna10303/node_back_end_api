999bf1c96d6c0d7104f6842f1e44fc47cfc4476e
Abarna Subramaniyan <112686551+abarna10303@users.noreply.github.com>
2024-12-21 02:52:05 +0530

    Update agriTechRouter.js

--- routers/agriTechRouter.js
+++ routers/agriTechRouter.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import {
   agriTechController,
+  otpLoginController,
+  otpLoginValidationController,
   postUserDataController,
+  postJobDetailsController,
 } from "../controllers/agriTechController.js";
 
 const router = Router();
 
 router.get("/getData", agriTechController);
 router.post("/updateData", postUserDataController);
+
+router.put("/otp-login", otpLoginController);
+router.put("/otp-login-validation", otpLoginValidationController);
+router.post("/create-job", postJobDetailsController);
 
 // router.get("/authenticate", azureAuthorizeController);
 
